f469feeb6c73ce42822fd51d72ecb0751717c141
Zhambyl Gaziz <gazizzhambyl@gmail.com>
2018-07-11 11:22:59 +0600

    Map changed to img

--- src/pages/find/find.ts
+++ src/pages/find/find.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-declare var google:any;
+// declare var google:any;
 /**
  * Generated class for the FindPage page.
  *
@@ -15,21 +15,21 @@ declare var google:any;
   templateUrl: 'find.html',
 })
 export class FindPage {
-  @ViewChild('map') mapRef:ElementRef;
+  // @ViewChild('map') mapRef:ElementRef;
   ordered: boolean = true;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
   ionViewDidLoad() {
-    this.DisplayMap();
+    // this.DisplayMap();
   }
-  DisplayMap(){
-    const location = new google.maps.LatLng('50.283934', '57.166978');
-    const options = {
-      center: location,
-      zoom: 10
-    }
-    const map = new google.maps.Map(this.mapRef.nativeElement, options);
-  }
+  // DisplayMap(){
+  //   const location = new google.maps.LatLng('50.283934', '57.166978');
+  //   const options = {
+  //     center: location,
+  //     zoom: 10
+  //   }
+  //   const map = new google.maps.Map(this.mapRef.nativeElement, options);
+  // }
   doRefresh(refresher){
     setTimeout(() => {
       console.log('Async operation has ended');
